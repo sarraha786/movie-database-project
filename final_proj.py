d563d92669edecf77b331b3178aef30c5f2f2b44
ktlynger <98409564+ktlynger@users.noreply.github.com>
2022-04-23 10:57:10 -0400

    youtube api

--- final_proj.py
+++ final_proj.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import os
 import sqlite3
 import unittest
+import youtube_dl
 from xml.sax import parseString
+import googleapiclient.discovery
 
 from bs4 import BeautifulSoup
 import requests
@@ -64,13 +66,38 @@ def movies_table(data,cur,conn):
 #        cur.execute('INSERT OR IGNORE INTO Movies (Name,Year,Rating) VALUES (?,?,?)', (name, year,rating))
 #    conn.commit()
 
+# test 
+
 
       
             
+# Youtube API---> using the API to get number of views, ratings, etc
 
-#def get_youtube_info():
-
-
+def get_youtube_info(movie_name):
+    API_KEY = 'AIzaSyAD2NINir17W0mXfiWF91_BdI-XYvbZwKc'
+    youtube_api = googleapiclient.discovery.build("youtube", "v3", developerKey = API_KEY)
+    trailer_name = movie_name + ' Trailer'
+    # api request 1 to get the trailer video info for each movie on the top 250 list
+    request = youtube_api.search().list(
+        part="id,snippet",
+        type='video',
+        q=trailer_name,
+        maxResults=1)
+    response = request.execute()
+    video_id = response['items'][0]['id']['videoId']
+    
+    # api request 2 to get the video statistics (viewcount, likecount, favoritecount, commentcount)
+    request = youtube_api.videos().list(
+        part='snippet,contentDetails,statistics',
+        id=video_id)
+    response2 = request.execute()
+    # trailer's statistics on youtube 
+    viewcount = response2['items'][0]['statistics']['viewCount']
+    likecount = response2['items'][0]['statistics']['likeCount']
+    favoritecount = response2['items'][0]['statistics']['favoriteCount']
+    commentcount = response2['items'][0]['statistics']['commentCount']
+    return (viewcount, likecount, favoritecount, commentcount)
+    
 
 #This function is going to search each movie +trailer, get stats: likes, dislikes, views, comments
 
@@ -88,6 +115,7 @@ def main():
     cur, conn = setUpDatabase('final_project_db.db')
     movie_tuples=get_top_movies()
     movies_table(movie_tuples, cur, conn)
+    get_youtube_info()
 main()
 #class TestCases(unittest.TestCase):
     
